f56af8f6d71fd23e67c48612dc0d25db76d5c9d8
austin mcdonald <austinmcdonald@austins-MacBook-Pro-2.local>
2020-02-28 14:54:34 -0600

    added pendilum notebook

--- pmnsnet.py
+++ pmnsnet.py
@@ -2,8 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import torch.optim as optim
-
-from torch.utils.data import Dataset,DataLoader
+import torch.utils.data as data
 
 import numpy as np
 import math
